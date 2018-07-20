9407fd6d8534206f10eebb22fab16b826920cf05
Daniel Zhang <zhang.ling1@husky.neu.edu>
2018-07-19 21:36:21 -0400

    Initial project

---
+++ app/src/main/java/neu/edu/runningsquad/GroupSearchActivity.java
@@ -0,0 +1,15 @@
+
+
+package neu.edu.runningsquad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_search);
+    }
+}
