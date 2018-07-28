41f174d2cd2ec7b7dd2eff9ce63fc4df7e18fa35
Xin Li <cug.ie.lixin@gmail.com>
2018-07-28 17:01:37 -0400

    init

--- app/src/main/java/neu/edu/runningsquad/MainActivity.java
+++ app/src/main/java/neu/edu/runningsquad/MainActivity.java
@@ -1,13 +1,69 @@
 package neu.edu.runningsquad;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private NavigationView navigationView;
+    protected FrameLayout contentFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        contentFrameLayout = findViewById(R.id.content_frame);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.baseline_menu_white_18);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        Log.i("Runner", "Main");
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+
+
+                    case R.id.nav_profile:
+                        Intent loadProfile = new Intent(getApplicationContext(), PersonalActivity.class);
+                        startActivity(loadProfile);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_groups:
+                        Intent loadGroups = new Intent(getApplicationContext(), GroupSearchActivity.class);
+                        startActivity(loadGroups);
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                }
+                return false;
+            }
+        });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
