37e0c66e15abdef0642655fb6530944bf9ec6e08
Xin Li <cug.ie.lixin@gmail.com>
2018-08-09 10:40:04 -0400

    personal interface

--- app/src/main/java/neu/edu/runningsquad/PersonalActivity.java
+++ app/src/main/java/neu/edu/runningsquad/PersonalActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,11 +20,27 @@ public class PersonalActivity extends MainActivity {
     private String username;
     private DatabaseReference mReference;
 
+    private TextView emailTextView;
+    private TextView usernameTextView;
+    private TextView starTextView;
+
+    private TextView groupNameTextView;
+    private TextView groupStarsTextView;
+    private TextView groupRankingTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getLayoutInflater().inflate(R.layout.activity_personal, contentFrameLayout);
         username = Sessions.getUsername(this.getApplicationContext());
+
+
+        emailTextView = findViewById(R.id.personal_email);
+        usernameTextView = findViewById(R.id.personal_username);
+        starTextView = findViewById(R.id.personal_stars);
+        groupNameTextView = findViewById(R.id.personal_group_name);
+        groupStarsTextView = findViewById(R.id.personal_group_stars);
+        groupRankingTextView = findViewById(R.id.personal_group_ranking);
 
         mReference.child("users/" + username).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -40,6 +57,40 @@ public class PersonalActivity extends MainActivity {
 
     private void showUserInfo(User user){
 
+        emailTextView.setText(user.getEmail());
+        usernameTextView.setText(user.getEmail());
+        starTextView.setText(Integer.toString(user.getStar()));
+
+        String squad = user.getSquad();
+        initGroupData(squad);
+
+
+    }
+
+    public void initGroupData(String squadname) {
+        try {
+            mReference.child("squads").child(squadname).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Squad squad = dataSnapshot.getValue(Squad.class);
+                    showSquadInfo(squad);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+
+        } catch (Exception e) {
+            System.err.println("Getting squadData failed: " + e.getMessage());
+        }
+    }
+
+    public void showSquadInfo(Squad squad){
+
+        groupNameTextView.setText(squad.getName());
+        groupRankingTextView.setText("0");
+        groupStarsTextView.setText(String.valueOf(squad.getPrizes()));
     }
 
 }
