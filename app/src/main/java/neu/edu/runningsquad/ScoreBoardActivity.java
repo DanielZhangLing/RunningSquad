bd2e0c798658148d99e9626d6101c6ee9708b488
Xin Li <cug.ie.lixin@gmail.com>
2018-08-09 17:09:30 -0400

    push

--- app/src/main/java/neu/edu/runningsquad/ScoreBoardActivity.java
+++ app/src/main/java/neu/edu/runningsquad/ScoreBoardActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -52,7 +53,7 @@ public class ScoreBoardActivity extends MainActivity {
             public boolean onEditorAction(TextView v, int actionId,
                                           KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_GO) {
-
+                    Log.i("runningsquad", v.getText().toString());
                     searchForGroups(v.getText().toString());
                 }
                 return false;
@@ -76,7 +77,7 @@ public class ScoreBoardActivity extends MainActivity {
 
 
     void searchForGroups(String city){
-        mReference.child("squads").orderByChild("city").equalTo(city).orderByChild("score").addValueEventListener(new ValueEventListener() {
+        mReference.child("squads").orderByChild("city").equalTo(city).orderByChild("totalStars").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
